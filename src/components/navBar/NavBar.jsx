ce1b93d2621fe421fb045cf1dba7a2b737b224f9
ryan2625 <ryanfreas19@augustana.edu>
2023-07-01 18:47:40 -0500

    Adding navbar for computers

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavbarBrand, Navbar, Nav, Container} from 'react-bootstrap'
 import "./navBar.css"
+import logoCar from "./logoCar.png"
+import BoltIcon from '@mui/icons-material/Bolt';
 function NavBar() {
   return (
     <Navbar bg="light" data-bs-theme="light">
     <Container>
-      <Navbar.Brand href="#home">Nav<img src="logoCar.png" alt="" id="brandPicture"/></Navbar.Brand>
-      <Nav className="me-auto nav-Adjust">
+      <Navbar.Brand href="#home"><BoltIcon/><img src={logoCar} alt="" id="brandPicture"/></Navbar.Brand>
+      <Nav className="me-auto" id="nav-Adjust">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
