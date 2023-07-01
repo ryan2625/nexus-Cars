ac4b625079ddcbdd12d428a277f4a02a49563913
ryan2625 <ryanfreas19@augustana.edu>
2023-06-30 19:29:33 -0500

    Adding base files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import NavBar from './components/navBar/NavBar';
+import Home from './components/masterPage/Home';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <NavBar/>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+                {/*<Route path="/contacts" element={<Contacts/>}/> */}
+            </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>navBar</div>
+  )
+}
+
+export default NavBar
