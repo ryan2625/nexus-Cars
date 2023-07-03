d35a66d157aafd772c19a654dd18672d2250f298
ryan2625 <ryanfreas19@augustana.edu>
2023-07-03 15:07:50 -0500

    Adding underline style to navigation

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,19 +4,30 @@ import "./navBar.css"
 import { useState, useEffect } from 'react'
 import logoCar from "./logoCar.png"
 import BoltIcon from '@mui/icons-material/Bolt';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 function NavBar() {
 
   return (
-    <Navbar  collapseOnSelect expand="md" bg="light" data-bs-theme="light">
+    <Navbar  collapseOnSelect expand="lg" bg="light" data-bs-theme="light">
     <Container className='navBar1'>
       <Navbar.Brand href="#home"><BoltIcon/><img src={logoCar} alt="" id="brandPicture"/></Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse  className="adjust-Responsive-Nav" id="responsive-navbar-nav">
       <Nav className="me-auto" id="nav-Adjust">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Form className="d-flex" id="searchBar">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="light">Search</Button>
+          </Form>
+        <Nav.Link className="afterNav underLine" href="#home">Home</Nav.Link>
+        <Nav.Link className="afterNav underLine" href="#features">Features</Nav.Link>
+        <Nav.Link className="afterNav underLine" href="#pricing">Pricing</Nav.Link>
       </Nav>
       </Navbar.Collapse>
     </Container>
