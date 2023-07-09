3bef524d1ac6d460a758aa68fea91452d4a70d9e
ryan2625 <ryanfreas19@augustana.edu>
2023-07-09 16:50:23 -0500

    finished animation text

--- src/components/0thScrollBanner/ZeroBanner.jsx
+++ src/components/0thScrollBanner/ZeroBanner.jsx
@@ -12,9 +12,39 @@ function ZeroBanner() {
         rootMargin: '-150px 0px'
     });
 
+    const h2refer = useRef();
+
+    const [width2, setWidth] = useState(0);
+
+    const [widthScreen, setWidthScreen] = useState(0);
+
+    useEffect(() => {
+      const logSize = () => {
+        setWidthScreen(window.innerWidth);
+      }
+      setWidthScreen(window.innerWidth)
+      window.addEventListener("resize", logSize);
+    }, [window.innerWidth]);
+
+    useEffect(() => {
+      const getWidth = () => {
+        if (h2refer.current) {
+          const width = h2refer.current.offsetWidth;
+          setWidth(width)
+          
+        }
+      };
+      getWidth();
+      
+        h2refer.current.style.setProperty('--h2-width', `${width2}px`);
+      
+      console.log('Element width:', h2refer.current.offsetWidth);
+
+    }, [widthScreen]);
+
   return (<>
     <div className={offersVisible ? "noneDisplay  " + "offersAnimation" : "noneDisplay " + "offersAnimation2"} ref={currentOffers}>
-        <h2>
+        <h2 ref={h2refer}>
             VEHICLES AND CURRENT OFFERS
             </h2>
     </div>
