861a21c4bfd93a7e11e7ec6ead8f5c7ed0b8ea9c
ryan2625 <ryanfreas19@augustana.edu>
2023-07-14 11:29:37 -0500

    Adding next banner

--- src/components/secondScrollBanner/SecondScroll.jsx
+++ src/components/secondScrollBanner/SecondScroll.jsx
@@ -5,9 +5,45 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useRef, useEffect, useState} from 'react';
 import { useInView } from 'react-intersection-observer';
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+
+
 function SecondScroll() {
   return (
-    <div className='scrollBanner1'>SecondScroll</div>
+    <div className='scrollBanner1'>
+        <div className="secondContainer">
+            <div className="firstColumn" id='column1'>
+                <EditCalendarIcon style={{scale: "2"}} className='hoverIcon'/>
+                <h2>
+                    SCHEDULE A TEST DRIVE
+                </h2>
+                <p>
+                Enjoy an in-retailer private fitting room or bring the showroom to you.
+                </p>
+            </div>
+            <div className="firstColumn" id='column2'>
+            <ChatBubbleOutlineIcon style={{scale: "2"}} className='hoverIcon'/>
+            <h2>
+            CHAT WITH A CONCIERGE
+                </h2>
+                <p>
+                Ask our highly trained professionals any questions you may have.
+                </p>
+            </div>
+            <div className="firstColumn" id='column3'>
+            <PendingActionsIcon style={{scale: "2"}} className='hoverIcon'/>
+            <h2>
+            CUSTOM ORDERS
+                </h2>
+                <p>
+                Select your vehicle options and have your perfect car delivered to you.
+                </p>
+            </div>
+        </div>
+    </div>
+
   )
 }
 
