a6a064a196b42c737dac4dc1364304f0e7b32bf2
ryan2625 <ryanfreas19@augustana.edu>
2023-07-18 13:47:12 -0500

    Minor bug fixes

--- src/components/thirdScrollBanner/Thankyou.jsx
+++ src/components/thirdScrollBanner/Thankyou.jsx
@@ -10,8 +10,11 @@ function Thankyou() {
 
   return (
   <div className='thankYouContainer'>
-    <div className={thanksVisible ? "thankYou1" : "thankYou1 thankYou1a"} ref={thankYou}>THANK <span className={thanksVisible ? "thankYouColored" : "thankYouColored thankYouColoreda"} >YOU</span> <span className={thanksVisible ? "thankYou3" : "thankYou3a"}> FOR YOUR INTEREST IN NEXUS.</span> </div>
-    <div className={thanksVisible ? "thankYou2" : "thankYou2 thankYou2a"}>Some models, trims and features may not be available. Please contact your local Nexus Retailer for updates and assistance.</div>
+    <div className="refererer"  ref={thankYou}>
+    <div className={thanksVisible ? "thankYou1" : "thankYou1 thankYou1a"}>THANK <span className={thanksVisible ? "thankYouColored" : "thankYouColored thankYouColoreda"} >YOU</span> <span className={thanksVisible ? "thankYou3" : "thankYou3a"}> FOR YOUR INTEREST IN NEXUS.</span> </div>
+
+    <div className={thanksVisible ? "thankYou2" : "thankYou2 thankYou2a"} >Some models, trims and features may not be available. Please contact your local Nexus Retailer for updates and assistance.</div>
+    </div>
   </div>
   )
 }
